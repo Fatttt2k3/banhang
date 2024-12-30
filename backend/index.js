00bb25b45040e12e9ddb929f5c28c1f5e4234da8
Fatttt2k3 <fat290903@gmail.com>
2024-12-30 15:15:28 +0700

    cuu be

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+
+
+const app = express()
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://192.168.1.51:3000'] || process.env.FRONTEND_URL, // Liệt kê tất cả các nguồn được phép
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api",router)
+
+const PORT = 8080 || process.env.PORT
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
